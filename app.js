161a872d2b6fe7deedd2d73a814b11831cd2ef69
EC2 Default User <twodoorbells@gmail.com>
2019-12-20 21:33:57 +0000

    PDF2Base64 initial build

---
+++ app.js
@@ -0,0 +1,35 @@
+const   express = require('express'),
+        bodyParser = require('body-parser'),
+        fs = require('fs'),
+        pdf2base64 = require('pdf-to-base64');
+        
+        
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let inputFile = "public/CTtest.pdf"
+let outputFile = "public/output/jsonOut.txt"
+
+pdf2base64(inputFile)
+    .then(
+        (response) => {
+            // console.log(response); //cGF0aC90by9maWxlLmpwZw==
+            fs.writeFile(outputFile, 
+                response, (err) => {
+                    if(err) console.log(err);
+                    console.log("file write success");
+                });
+        }
+    )
+    .catch(
+        (error) => {
+            console.log(error); //Exepection error....
+        }
+    )
+
+
+console.log("PORT: ", process.env.PORT);
+console.log("IP: ", process.env.IP);
+app.listen(process.env.PORT, process.env.IP, function(){
+        console.log("It's dead!");
+})
