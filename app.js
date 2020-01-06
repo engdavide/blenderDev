cf8b5530c87804c6da27f7665b078ba7171fc1c9
EC2 Default User <twodoorbells@gmail.com>
2020-01-06 22:33:44 +0000

    expand bodyparser limit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const   express = require('express'),
         
         
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.set("view engine", "ejs");
 
 var data = [
