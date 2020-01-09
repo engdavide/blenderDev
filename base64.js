e98a24a8994916d13635e60bb4ab042598cd46db
EC2 Default User <twodoorbells@gmail.com>
2020-01-09 13:03:17 +0000

    try url base64 encode only

--- base64.js
+++ base64.js
@@ -6,24 +6,33 @@
  *
  */
 
-var base64 = exports;
-
-base64.encode = function(unencoded) {
+function base64encode(unencoded) {
   return new Buffer(unencoded || '').toString('base64');
 };
 
-base64.decode = function(encoded) {
+function base64decode(encoded) {
   return new Buffer(encoded || '', 'base64').toString('utf8');
 };
 
-base64.urlEncode = function(unencoded) {
-  var encoded = base64.encode(unencoded);
-  return encoded.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-};
+module.exports = {
+  base64encode: function(unencoded) {
+    return new Buffer(unencoded || '').toString('base64');
+  },
+  
+  base64decode: function(encoded) {
+    return new Buffer(encoded || '', 'base64').toString('utf8');
+  },
+  
+  base64urlEncode: function(unencoded) {
+    var encoded = base64encode(unencoded);
+    return encoded.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+  },
+  
+  base64urlDecode: function(encoded) {
+    encoded = encoded.replace(/-/g, '+').replace(/_/g, '/');
+    while (encoded.length % 4)
+      encoded += '=';
+    return base64decode(encoded);
+  }
 
-base64.urlDecode = function(encoded) {
-  encoded = encoded.replace(/-/g, '+').replace(/_/g, '/');
-  while (encoded.length % 4)
-    encoded += '=';
-  return base64.decode(encoded);
-};
+}
